52f2cd9b50b36aedaaa867255567b34a5419fbe9
Frederic Wojcikowski <wojcikowskif@gmail.com>
2019-08-18 13:34:35 +0900

    Communication entre le fragment et l'activite

--- app/src/main/java/guide/developpement/myfragmentapp/Controllers/Activities/MainActivity.java
+++ app/src/main/java/guide/developpement/myfragmentapp/Controllers/Activities/MainActivity.java
@@ -1,15 +1,28 @@
 package guide.developpement.myfragmentapp.Controllers.Activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
+import guide.developpement.myfragmentapp.Controllers.Fragments.MainFragment;
 import guide.developpement.myfragmentapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // --------------
+    // CallBack
+    // --------------
+
+    @Override
+    public void onButtonClicked(View view) {
+        Log.e(getClass().getSimpleName(), "Button clicked !");
     }
 }
