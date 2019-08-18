7c343d302cbe7c979e45e5d45aedb5338472f2d9
Frederic Wojcikowski <wojcikowskif@gmail.com>
2019-08-18 16:38:04 +0900

    Optimiser l'affichage sur tablette
    End ep 9 & Start ep 10
    Ameliorez la stabilite de votre application

--- app/src/main/java/guide/developpement/myfragmentapp/Controllers/Activities/MainActivity.java
+++ app/src/main/java/guide/developpement/myfragmentapp/Controllers/Activities/MainActivity.java
@@ -3,24 +3,26 @@ package guide.developpement.myfragmentapp.Controllers.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 
+import guide.developpement.myfragmentapp.Controllers.Fragments.DetailFragment;
 import guide.developpement.myfragmentapp.Controllers.Fragments.MainFragment;
 import guide.developpement.myfragmentapp.R;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnButtonClickedListener {
 
-    // 1 - Declare main fragment
+    // 1 - Declare main and detail fragments.
     private MainFragment mMainFragment;
+    private DetailFragment mDetailFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 2 - Configure and show home fragment
+        // 2 - Configure and show main and detail fragments.
         this.configureAndShowMainFragment();
+        this.configureAndShowDetailFragment();
     }
 
     // --------------
@@ -29,15 +31,17 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnBu
 
     @Override
     public void onButtonClicked(View view) {
-        Log.e(getClass().getSimpleName(), "Button clicked !");
-        startActivity(new Intent(this, DetailActivity.class));
+        // 3 - Check if detail fragment is not created or if not visible
+        if (mDetailFragment == null || !mDetailFragment.isVisible()) {
+            startActivity(new Intent(this, DetailActivity.class));
+        }
     }
 
     // --------------
     // FRAGMENTS
     // --------------
 
-    private void configureAndShowMainFragment(){
+    private void configureAndShowMainFragment() {
         // A - Get FragmentManager (Support) and Try to find existing instance of fragment in FrameLayout container
         mMainFragment = (MainFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_main);
 
@@ -47,6 +51,18 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnBu
             // C - Add it to FrameLayout container
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.frame_layout_main, mMainFragment)
+                    .commit();
+        }
+    }
+
+    private void configureAndShowDetailFragment() {
+        mDetailFragment = (DetailFragment) getSupportFragmentManager().findFragmentById(R.id.frame_layout_detail);
+
+        // A - We only add DetailFragment in Tablet mode (If found frame_layout_detail)
+        if (mDetailFragment == null && findViewById(R.id.frame_layout_detail) != null) {
+            mDetailFragment = new DetailFragment();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.frame_layout_detail, mDetailFragment)
                     .commit();
         }
     }
